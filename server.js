4082e6683723d061ec84156d17f23d6892114c03
Duc Ngo <duc.ngo@interflour.com>
2024-02-16 09:23:25 -0500

    update

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require('express')
+require('dotenv')
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb+srv://ducngo:helloworld@cluster0.232lfkg.mongodb.net/Marketplace?retryWrites=true&w=majority', {useNewUrlParser: true })
+db.on()
+
+
+app.listen(3000, () => console.log("server has been started on port 3000 , dont worry so much!"))
